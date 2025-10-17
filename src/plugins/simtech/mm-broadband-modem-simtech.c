bc83cc5c9e41ec2938dd15b86b70b9906f3b1bf0
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2025-10-17 11:05:10 +0500

    Add SIMCOM A7600/A7602/A7608 USSD support
    * The modem expect data in CUSD command to GSM-7 encoded
    * The modem returns status 2, when successfully receiving an USSD answer from the provider. Similar behavior is described for Telit modem here https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/issues/103

--- src/plugins/simtech/mm-broadband-modem-simtech.c
+++ src/plugins/simtech/mm-broadband-modem-simtech.c
@@ -35,27 +35,31 @@
 #include "mm-iface-modem-3gpp.h"
 #include "mm-iface-modem-location.h"
 #include "mm-iface-modem-voice.h"
+#include "mm-iface-modem-3gpp-ussd.h"
 #include "mm-shared-simtech.h"
 #include "mm-broadband-modem-simtech.h"
 #include "gpio-helper.h"
 
-static void iface_modem_init          (MMIfaceModemInterface         *iface);
-static void iface_modem_3gpp_init     (MMIfaceModem3gppInterface     *iface);
-static void iface_modem_location_init (MMIfaceModemLocationInterface *iface);
-static void iface_modem_voice_init    (MMIfaceModemVoiceInterface    *iface);
-static void shared_simtech_init       (MMSharedSimtechInterface      *iface);
+static void iface_modem_init           (MMIfaceModemInterface          *iface);
+static void iface_modem_3gpp_init      (MMIfaceModem3gppInterface      *iface);
+static void iface_modem_location_init  (MMIfaceModemLocationInterface  *iface);
+static void iface_modem_voice_init     (MMIfaceModemVoiceInterface     *iface);
+static void iface_modem_3gpp_ussd_init (MMIfaceModem3gppUssdInterface  *iface);
+static void shared_simtech_init        (MMSharedSimtechInterface       *iface);
 
-static MMIfaceModemInterface         *iface_modem_parent;
-static MMIfaceModem3gppInterface     *iface_modem_3gpp_parent;
-static MMIfaceModemLocationInterface *iface_modem_location_parent;
-static MMIfaceModemVoiceInterface    *iface_modem_voice_parent;
+static MMIfaceModemInterface          *iface_modem_parent;
+static MMIfaceModem3gppInterface      *iface_modem_3gpp_parent;
+static MMIfaceModemLocationInterface  *iface_modem_location_parent;
+static MMIfaceModemVoiceInterface     *iface_modem_voice_parent;
+static MMIfaceModem3gppUssdInterface  *iface_modem_3gpp_ussd_parent;
 
 G_DEFINE_TYPE_EXTENDED (MMBroadbandModemSimtech, mm_broadband_modem_simtech, MM_TYPE_BROADBAND_MODEM, 0,
                         G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM, iface_modem_init)
                         G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM_3GPP, iface_modem_3gpp_init)
                         G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM_LOCATION, iface_modem_location_init)
                         G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM_VOICE, iface_modem_voice_init)
-                        G_IMPLEMENT_INTERFACE (MM_TYPE_SHARED_SIMTECH, shared_simtech_init))
+                        G_IMPLEMENT_INTERFACE (MM_TYPE_SHARED_SIMTECH, shared_simtech_init)
+                        G_IMPLEMENT_INTERFACE (MM_TYPE_IFACE_MODEM_3GPP_USSD, iface_modem_3gpp_ussd_init))
 
 typedef enum {
     FEATURE_SUPPORT_UNKNOWN,
@@ -1716,6 +1720,36 @@ setup_ports (MMBroadbandModem *self)
 }
 
 /*****************************************************************************/
+/* USSD encode/decode (3GPP-USSD interface)
+ *
+ * SIM A7600E-H/A7602E-H devices don't use the current charset (as per AT+CSCS) in the CUSD
+ * command, they instead expect data in ASCII with GSM-7 coding scheme.
+ */
+
+static gchar *
+modem_3gpp_ussd_encode (MMIfaceModem3gppUssd *self,
+                                        const gchar *command,
+                                        guint *scheme,
+                                        GError **error)
+{
+    if (g_str_has_prefix (mm_iface_modem_get_model (MM_IFACE_MODEM (self)), "A7600E-H") ||
+        g_str_has_prefix (mm_iface_modem_get_model (MM_IFACE_MODEM (self)), "A7602E-H") ||
+        g_str_has_prefix (mm_iface_modem_get_model (MM_IFACE_MODEM (self)), "A7608E-H")) {
+        *scheme = MM_MODEM_GSM_USSD_SCHEME_7BIT;
+        return mm_modem_charset_str_from_utf8 (command, MM_MODEM_CHARSET_GSM, FALSE, error);
+    }
+    return iface_modem_3gpp_ussd_parent->encode(self, command, scheme, error);
+}
+
+static gchar *
+modem_3gpp_ussd_decode (MMIfaceModem3gppUssd *self,
+                                        const gchar *reply,
+                                        GError **error)
+{
+    return iface_modem_3gpp_ussd_parent->decode(self, reply, error);
+}
+
+/*****************************************************************************/
 
 MMBroadbandModemSimtech *
 mm_broadband_modem_simtech_new (const gchar *device,
@@ -1864,6 +1898,15 @@ iface_modem_voice_init (MMIfaceModemVoiceInterface *iface)
     iface->cleanup_in_call_audio_channel        = mm_shared_simtech_voice_cleanup_in_call_audio_channel;
     iface->cleanup_in_call_audio_channel_finish = mm_shared_simtech_voice_cleanup_in_call_audio_channel_finish;
 
+}
+
+static void
+iface_modem_3gpp_ussd_init (MMIfaceModem3gppUssdInterface *iface)
+{
+    iface_modem_3gpp_ussd_parent = g_type_interface_peek_parent (iface);
+
+    iface->encode = modem_3gpp_ussd_encode;
+    iface->decode = modem_3gpp_ussd_decode;
 }
 
 static MMIfaceModemVoiceInterface *
